2ad54c916b73165319ac48f9fb84ff4aca83b80c
Ikonnikov <Ikonnikov.Oleg47@gmail.com>
2023-06-07 21:28:28 +0300

    new test

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -2,6 +2,7 @@ import time
 
 from webdriver_manager.core import driver
 
+from generator.generator import generated_person
 from locators.elements_page_locators import TextBoxPageLocators
 from pages.base_page import BasePage
 
@@ -9,11 +10,17 @@ class TextBoxPage(BasePage):
     locators = TextBoxPageLocators()
 
     def fill_all_fields(self):
-        self.element_is_visible(self.locators.FULL_NAME).send_keys("VITYA CHESNOK")
-        self.element_is_visible(self.locators.EMAIL).send_keys("Vitya@222.com")
-        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys("Luberci")
-        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys("Strogino")
+        person_info = next(generated_person)
+        full_name = person_info.full_name
+        email = person_info.email
+        current_address = person_info.current_address
+        permanent_address = person_info.permanent_address
+        self.element_is_visible(self.locators.FULL_NAME).send_keys(full_name)
+        self.element_is_visible(self.locators.EMAIL).send_keys(email)
+        self.element_is_visible(self.locators.CURRENT_ADDRESS).send_keys(current_address)
+        self.element_is_visible(self.locators.PERMANENT_ADDRESS).send_keys(permanent_address)
         self.element_is_visible(self.locators.SUBMIT).click()
+        return full_name, email, current_address, permanent_address
 
 
     def check_filled_form(self):
